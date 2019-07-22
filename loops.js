d4377b6a7e8bf8cf33d4a143c302b24a2e8f40c9
Katrina R <katrina.reinsdorff@gmail.com>
2019-07-22 20:53:41 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,33 @@
+function forLoop(array) {
+  for(var i = 0; i < 25; i++) {
+    if (i === 1) {
+      array.push("I am 1 strange loop.")
+    }
+    else {
+      array.push(`I am ${i} strange loops.`)
+    }
+  }
+  return(array);
+}
+
+function whileLoop(number){
+  var n = number;
+  while (n > 0)
+  {
+    console.log(n-=1);
+  }
+  return("done");
+}
+
+function doWhileLoop(integer) {
+  
+  function incrementVariable() {
+  i = integer - 1;
+  return i;
+}
+
+do {
+  console.log("I run once regardless.");
+} while (incrementVariable() < num);
+  
+}
